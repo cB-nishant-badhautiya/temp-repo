662c7f646c870fa74d9150c3c4a120009e914fe3
cB-nishant-badhautiya <nishant.b@thecodebucket.com>
2025-12-10 12:44:23 +0530

    second commit

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ console.log("Hello world")
 for(let i = 0; i < size; i++){
 	console.log(i);
 }
+
+function sum(a, b) {
+	return a + b;
+}
+
+const output = sum(3, 5)
+console.log(sum)
---
+++ main2.js
@@ -0,0 +1,1 @@
+console.log("This is main2.js file")
